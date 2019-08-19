bda9290e133716e10bd3d71b44c31a1ec651f318
Procarihana <48359478+Procarihana@users.noreply.github.com>
2019-08-20 00:26:10 +0800

    爬取GitHub的Pull request并存储为CSV文件 (#17)
     爬取GitHub的Pull request并存储为CSV文件 (#17)

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,11 +1,46 @@
 package com.github.hcsp.io;
 
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Crawler {
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
     // number,author,title
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
-    public static void savePullRequestsToCSV(String repo, int n, File csvFile) {}
+    public static void savePullRequestsToCSV(String repo, int n, File csvFile) throws IOException {
+        GitHub github = GitHub.connectAnonymously();
+        GHRepository repository = github.getRepository(repo);
+        List<GHPullRequest> pullRequests = repository.getPullRequests(GHIssueState.OPEN);
+
+        String title = "number,author,title";
+        List<String> lines = new ArrayList<>();
+        List<String> body = pullRequests.stream().map(Crawler::getLine).collect(Collectors.toList());
+        lines.add(title);
+        for (int i = 0; i < n; i++) {
+            lines.addAll(Collections.singleton(body.get(i)));
+        }
+
+
+        Files.write(csvFile.toPath(), lines);
+    }
+
+    static String getLine(GHPullRequest pullRequest) {
+        try {
+            return pullRequest.getNumber() + "," + pullRequest.getUser().getLogin() + "," + pullRequest.getTitle();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
