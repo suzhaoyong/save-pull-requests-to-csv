deb6fa321594613b0ad5d47932f9860fdb3ca476
Bo Zhang <zhangbodut@gmail.com>
2019-08-31 22:32:56 +0800

    Fetch fewer PRs

--- src/test/java/com/github/hcsp/io/CrawlerTest.java
+++ src/test/java/com/github/hcsp/io/CrawlerTest.java
@@ -14,14 +14,14 @@ public class CrawlerTest {
     @Test
     public void test() throws Exception {
         File tmp = File.createTempFile("csv", "");
-        Crawler.savePullRequestsToCSV("golang/go", 30, tmp);
+        Crawler.savePullRequestsToCSV("golang/go", 10, tmp);
 
         CSVReader reader = new CSVReader(new BufferedReader(new FileReader(tmp)));
         List<String[]> lines = reader.readAll();
         Assertions.assertArrayEquals(lines.get(0), new String[] {"number", "author", "title"});
-        Assertions.assertTrue(lines.size() > 30);
+        Assertions.assertTrue(lines.size() > 10);
 
-        String[] lastLine = lines.get(30);
+        String[] lastLine = lines.get(10);
         GHPullRequest pull =
                 GitHub.connectAnonymously()
                         .getRepository("golang/go")
