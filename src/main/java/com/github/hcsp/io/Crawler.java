6617b48fbab6cd8b5def8ba2737f84f85dcccb3a
hukaibo <52575259+hukaibo@users.noreply.github.com>
2019-09-16 21:58:48 +0800

    修改 (#27)
    修改 (#27)

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,11 +1,84 @@
 package com.github.hcsp.io;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Crawler {
+    static class GitHubPullRequest {
+        // Pull request的编号
+        String number;
+        // Pull request的标题
+        String title;
+        // Pull request的作者的GitHub id
+        String author;
+
+        GitHubPullRequest(String number, String title, String author) {
+            this.number = number;
+            this.title = title;
+            this.author = author;
+        }
+    }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
     // number,author,title
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
-    public static void savePullRequestsToCSV(String repo, int n, File csvFile) {}
+
+
+    public static void savePullRequestsToCSV(String repo, int n, File csvFile) throws IOException {
+        List<GitHubPullRequest> pullRequestList = getFirstPageOfPullRequests(repo);
+        List<String> lines = new ArrayList<>();
+        lines.add("number,author,title");
+        for (int i = 0; i < n; ++i) {
+            GitHubPullRequest pr = pullRequestList.get(i);
+            lines.add(pr.number + "," + pr.author + "," + pr.title);
+        }
+        FileUtils.writeLines(csvFile, lines);
+    }
+
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> gitHubPullRequests = new ArrayList<>();
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://github.com/" + repo + "/pulls");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+            String html = IOUtils.toString(is, "UTF-8");
+            Document doc = Jsoup.parse(html);
+            ArrayList<Element> issues = doc.select(".js-issue-row");
+            for (Element element : issues) {
+                String[] authorAndNumber = element.select(".opened-by").text().split(" ");
+                String number = authorAndNumber[0].replace("#", "");
+                String author = authorAndNumber[authorAndNumber.length - 1];
+                String title = element.child(0).child(1).child(0).text();
+
+                gitHubPullRequests.add(new GitHubPullRequest(number, title, author));
+            }
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return gitHubPullRequests;
+    }
+
+
 }
