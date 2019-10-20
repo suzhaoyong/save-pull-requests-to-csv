4a8ce5d846c00bc56ef89309d8fca3bfc885b635
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-10-20 11:11:57 +0800

    爬取GitHub的Pull request并存储为CSV文件 (#42)
     爬取GitHub的Pull request并存储为CSV文件 (#42)

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,11 +1,35 @@
 package com.github.hcsp.io;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.kevinsawicki.http.HttpRequest;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Crawler {
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
     // number,author,title
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
-    public static void savePullRequestsToCSV(String repo, int n, File csvFile) {}
+
+    public static void savePullRequestsToCSV(String repo, int n, File csvFile) throws IOException {
+        String response = HttpRequest.get("https://api.github.com/repos/" + repo + "/pulls").body();
+        JSONArray pulls = JSON.parseArray(response);
+        List<String> list = new ArrayList<>();
+        int length = Math.min(pulls.size(), n);
+        list.add("number,author,title");
+        for (int i = 0; i < length; i++) {
+            JSONObject pr = pulls.getJSONObject(i);
+            Integer number = pr.getInteger("number");
+            String title = pr.getString("title");
+            String author = pr.getJSONObject("user").getString("login");
+            list.add(number + "," + author + "," + title);
+        }
+        Files.write(csvFile.toPath(), list);
+    }
 }
