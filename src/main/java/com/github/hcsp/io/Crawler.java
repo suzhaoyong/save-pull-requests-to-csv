2f3cd33d9ae6d0f2c3257681032b22ac8084feff
maxiaoda <48616409+maxiaoda@users.noreply.github.com>
2020-03-01 16:39:55 +0800

    IO实战: 爬取GitHub的Pull request并存储为CSV文件 (#86)
    IO实战: 爬取GitHub的Pull request并存储为CSV文件 (#86)

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,11 +1,37 @@
 package com.github.hcsp.io;
 
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class Crawler {
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
     // number,author,title
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
-    public static void savePullRequestsToCSV(String repo, int n, File csvFile) {}
+    public static void savePullRequestsToCSV(String repo, int n, File csvFile) throws IOException {
+        GitHub github = GitHub.connectAnonymously();
+        GHRepository repository = github.getRepository(repo);
+        List<GHPullRequest> ghPullRequestList = repository.getPullRequests(GHIssueState.OPEN);
+
+        //write file
+        String cvsFileContent = "number,author,title\n";
+        for (int i = 0; i < n; i++) {
+            int number = ghPullRequestList.get(i).getNumber();
+            String author = ghPullRequestList.get(i).getUser().getLogin();
+            String title = ghPullRequestList.get(i).getTitle();
+
+            String lines = number + "," + author + "," + title + "\n";
+            cvsFileContent += lines;
+        }
+
+        Files.write(csvFile.toPath(), cvsFileContent.getBytes());
+    }
 }
+
